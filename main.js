a645c38bc3ecfa0686d2f203f8e9f6d700b29025
VahanHovanisyan <109031690+VahanHovanisyan@users.noreply.github.com>
2024-06-27 16:37:23 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-function slider({ elem, slidePerView = 1, slideToScroll = 1 }) {
+function slider(elem, pagination = false) {
   const slider = document.querySelector(`[data-slider="${elem}"]`);
   const sliderList = slider.querySelector('.slider__list');
   const sliderSlides = slider.querySelectorAll('.slider__slide');
   const sliderButtonPrev = slider.querySelector('.slider__button-prev');
   const sliderButtonNext = slider.querySelector('.slider__button-next');
 
-  sliderSlides.forEach((slide) => {
-    slide.style.minWidth = `calc(100% / ${slidePerView})`;
+  if (pagination) {
+    const sliderPagination = slider.querySelector('.slider__pagination');
 
+    for (let i = 0; i < sliderSlides.length; i += 1) {
+
+      const paginationButton = document.createElement('button');
+      paginationButton.innerHTML = i + 1;
+      paginationButton.classList.add('slider__pagination-button');
+
+      sliderPagination.append(paginationButton);
+
+      paginationButton.onclick = () => {
+        sliderList.scrollLeft = i * sliderList.offsetWidth;
+      };
+    }
+  }
+
+  sliderSlides.forEach((slide) => {
     sliderButtonPrev.onclick = () => {
-      sliderList.scrollBy(-slide.offsetWidth * slideToScroll, 0);
+      sliderList.scrollBy(-slide.offsetWidth * +slider.computedStyleMap().get('--slider-per-view')[0], 0);
     };
 
     sliderButtonNext.onclick = () => {
-      sliderList.scrollBy(slide.offsetWidth * slideToScroll, 0);
+      sliderList.scrollBy(slide.offsetWidth * +slider.computedStyleMap().get('--slider-per-view')[0], 0);
     };
   });
 }
 
-slider({
-  elem: 'slider1',
-  slidePerView: 2,
-  slideToScroll: 2,
-});
+slider('slider1', true);
